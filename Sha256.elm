7d123653743d063a549cff88b0d8e5c3e745db98
Bill St. Clair <billstclair@gmail.com>
2016-10-12 07:27:10 -0400

    First commit

---
+++ Sha256.elm
@@ -0,0 +1,31 @@
+----------------------------------------------------------------------
+--
+-- sha256.elm
+-- sha256 & sha224 for Elm
+-- Copyright (c) 2016 Bill St. Clair <billstclair@gmail.com>
+-- Some rights reserved.
+-- Distributed under the MIT License
+-- See LICENSE.txt
+--
+----------------------------------------------------------------------
+
+module Sha256 exposing (sha256, sha224)
+
+{-| This module makes available a JavaScript implementation of the
+sha256 and sha224 crytographic hash functions.
+
+# Functions
+@docs sha256, sha224
+-}
+
+import Native.Sha256
+
+{-| Returns the sha256 hash of its argument.
+-}
+sha256 : String -> String
+sha256 = Native.Sha256.sha256
+
+{-| Returns the sha224 hash of its argument.
+-}
+sha224 : String -> String
+sha224 = Native.Sha256.sha224
---
+++ example.elm
@@ -0,0 +1,50 @@
+import Sha256 exposing (sha256, sha224)
+
+import Html exposing (Html, div, p, text)
+import Html.Attributes exposing (style)
+import String
+import Char
+
+strings : List String
+strings =
+  [ "foo"
+  , "The quick brown fox jumped over the lazy dog."
+  , "Four score and seven years ago..."
+  ]
+
+stringFromCode : Int -> String
+stringFromCode code =
+  String.cons (Char.fromCode code) ""
+
+nbsp : String
+nbsp = stringFromCode 160   -- \u00A0
+
+br : Html a
+br = Html.br [] []
+
+render : String -> String -> String -> Html a
+render string hashName hash =
+  div [ style [ ("font-family", "'Courier New', Courier, monospace") ] ]
+    [ text (hashName ++ "(\"" ++ string ++ "\") = ")
+    , br
+    , text (nbsp ++ nbsp ++ "\"" ++ hash ++ "\"")
+    , br
+    ]
+
+render256 : String -> Html a
+render256 string =
+  -- ** Don't blink, here's the sha256 call. **
+  render string "sha256" (sha256 string)
+
+render224 : String -> Html a
+render224 string =
+  -- ** Don't blink, here's the sha224 call. **
+  render string "sha224" (sha224 string)
+
+main =
+  div []
+    [ p []
+        (List.map render256 strings)
+    , p []
+        (List.map render224 strings)
+    ]
